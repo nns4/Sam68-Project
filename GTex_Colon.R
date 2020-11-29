8da62d3ba3c921bb2b2bb72fecd53a5cfd2d4faa
nns4 <74169203+nns4@users.noreply.github.com>
2020-11-29 06:27:41 +0000

    Update GTex_Colon.R

--- GTex_Colon.R
+++ GTex_Colon.R
@@ -1,30 +1,32 @@
 
-
 #https://rstudio-pubs-static.s3.amazonaws.com/240657_5157ff98e8204c358b2118fa69162e18.html
 #install.packages("WGCNA")
 #BiocManager::install("impute")
 library(WGCNA)
 
-# read the file in GTex_data 
-GTex_data <- read.csv("GTex_Colon_TPMs.csv")
+#BiocManager::install("CePa")
+library(CePa)
+
+# read the file in GTex_data it takes time wait for a while
+GTex_data <- read.gct("GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_median_tpm.gct")
+
 options(scipen = 999)
+GTex_data <- data.frame(GTex_data)
 
 # finding the position of KHDRBS gene
-grep("KHDRBS",GTex_data$Gene)
+grep("ENSG00000121774",rownames(GTex_data))
+# GeneID for KHDRBS gene
+GTex_data[grep("ENSG00000121774",rownames(GTex_data)),]
 
 
 # transpose the data (columns to rows and vice-versa)
 GTex_transpose <- transposeBigData(GTex_data)
-GTex_transpose[2,20803]
 
-# remove ID and gene columns from data 
-GTex_newT <- GTex_transpose[3:nrow(GTex_transpose),]
-colnames(GTex_newT) <- GTex_data$Gene
 
-# add KHDRBS2 gene column to starting of data
-GTex_newSub = subset(GTex_newT, select = -c(`KHDRBS2-OT1`) )
-KHDRBS2_OT1<-GTex_newT[,"KHDRBS2-OT1"]
-GTex <- cbind(KHDRBS2_OT1,GTex_newSub)
+# add KHDRBS1 gene column to starting of data
+GTex_newSub = subset(GTex_transpose, select = -c(`ENSG00000121774.17`) )
+ENSG00000121774.17<-GTex_transpose[,"ENSG00000121774.17"]
+GTex <- cbind(ENSG00000121774.17,GTex_newSub)
 # convert data to numeric type 
 GTex <- sapply( GTex, as.numeric )
 
@@ -38,10 +40,22 @@ for (i in 1:ncol(GTex)){
   estimates[i] = test$estimate
   pvalues[i] = test$p.value
 }
+
 df$Cor <- estimates
 df$P.value <- pvalues
-# this will contain correlation and p-value for each gene with KHDRBS2
-df
-write.csv(df,file = "df_new.csv")
+# this will contain correlation and p-value for each gene with KHDRBS1
+colnames(df) <- c("Gene","Cor","pvalue_GTex")
+df$Gene<-sub("\\.\\d+$", "", df$Gene)
 
+# plot 
+hist(df[,"Cor"],col="#7FFFD4",main = "Correlation Plot",xlab = "Cor")
+cor.test()
 
+# no. of genes with p-value less than 0.05
+dfp <- df[df$pvalue_GTex < 0.05,]
+dfp <- na.omit(dfp) ## count for  no. of genes with p-value less than 0.05 
+
+# no. of genes with correaltion greater than 0.5 with sam68
+dfcoeg <- dfp[dfp$Cor > 0.5,]
+# no. of genes with correaltion less than 0.5 with sam68
+dfcoel <- dfp[dfp$Cor < 0.5,]
