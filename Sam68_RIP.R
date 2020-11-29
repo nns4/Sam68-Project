f810b5faf522fd9f2ed390a2143739c4a0615da2
nns4 <74169203+nns4@users.noreply.github.com>
2020-11-29 06:31:28 +0000

    Update Sam68_RIP.R

--- Sam68_RIP.R
+++ Sam68_RIP.R
@@ -1,20 +1,28 @@
 #https://replicationindex.com/2019/03/31/one-tail-or-two-tails-that-is-the-question/
 ## Calculating P-values
+library(dplyr)
+library(magrittr)
+library(stringr)
+library(ggplot2)
+## Calculating P-values
 options(scipen = 999)
 
 # load the data in S_sam68 variable
-S_sam68 <- read.csv("S_sam68_RIP_WC.csv")
-names(S_sam68[,4:6])
+S_sam68_RIP <- read.csv("S_sam68_RIP_WC.csv")
 
 # calculating mean of 3 control columns for each gene 
-S_sam68["control_mean"]<-rowMeans(S_sam68[,4:6])
+S_sam68_RIP["control_mean"]<-rowMeans(S_sam68_RIP[,4:6])
 # calculating sd of 3 control columns for each gene
-S_sam68["control_sd"] <-apply(S_sam68[,4:6], 1, sd)
-S_sam68$control_sd<- round(S_sam68$control_sd,3)
+S_sam68_RIP["control_sd"] <-apply(S_sam68_RIP[,4:6], 1, sd)
 
 # calculating  Right tail p-values pnorm() function 
-p_value <- pnorm(S_sam68$Sam68,mean = S_sam68$control_mean,sd = S_sam68$control_sd,lower.tail = FALSE)
-S_sam68["p_value"] <- p_value
-S_sam68$p_value<- round(S_sam68$p_value,5)
+p_value <- pnorm(S_sam68_RIP$Sam68,mean = S_sam68_RIP$control_mean,sd = S_sam68_RIP$control_sd,lower.tail = FALSE)
+S_sam68_RIP["p_value_Sam68_RIP"] <- p_value
+S_sam68_RIP["fdr"] <- p.adjust(S_sam68_RIP$p_value)
 
-write.csv(S_sam68, file = "Sam68_RIP_results.csv")
+# subset the gene name
+S_sam68_RIP$Gene<-sub("\\.\\d+$", "", S_sam68_RIP$Gene)
+
+
+# No. of significant gene with p-value less than 0.05
+S_sam68_sig <- S_sam68_RIP[S_sam68_RIP$p_value_Sam68_RIP <0.05,]
